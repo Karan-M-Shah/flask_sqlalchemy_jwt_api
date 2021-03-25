1c66b1920d673c45aae33b233c55677b418dad34
Karan-M-Shah <35928178+Karan-M-Shah@users.noreply.github.com>
2021-03-24 22:29:00 -0400

    Delete web directory

--- web/db.py
+++
@@ -1,5 +0,0 @@
-from flask_sqlalchemy import SQLAlchemy
-
-# An object of type SQLAlchemy. This links to our Flask app and allows us to map our objects to rows in a database
-# For example, our ItemModel object with a name and price column can easily be placed into a database
-db = SQLAlchemy()
